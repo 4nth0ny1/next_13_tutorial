bf8793e9490893fe2eb0e56b0b6926f545a831f3
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-30 15:40:21 -0500

    added Header Component and imported to Layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
+import Header from "./components/Header";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Header />
         <main className="container">{children}</main>
       </body>
     </html>
