159513810d4527a02ee4b00811a0094f74c83eae
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-30 16:10:26 -0500

    added ui for repos page

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
 async function fetchRepos() {
-  const response = await fetch("https://api.github.com/users/4nth0ny1/repos");
+  const response = await fetch("https://api.github.com/users/4nth0ny1/repos", {
+    next: { revalidate: 60 },
+  });
   const repos = await response.json();
   return repos;
 }
@@ -7,10 +12,33 @@ async function fetchRepos() {
 const ReposPage = async () => {
   const repos = await fetchRepos();
   // console.log(repos); // since this is a server component, this console.log() will not show up in the client (browser), it will show up in the terminal.
+
   return (
-    <div>
-      <h2>ReposPage</h2>
-      {repos[0].name}
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => {
+          return (
+            <li key={repo.id}>
+              <Link href={`code/repos/${repo.name}`}>
+                <h3>{repo.name}</h3>
+                <p>{repo.description}</p>
+                <div className="repo-details">
+                  <span>
+                    <FaStar /> {repo.stargazers_count}
+                  </span>
+                  <span>
+                    <FaCodeBranch /> {repo.forks_count}
+                  </span>
+                  <span>
+                    <FaEye /> {repo.watchers_count}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
