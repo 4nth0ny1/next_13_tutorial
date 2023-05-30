c5bb63d19094c834bb96f67416ca068d7dbcba4c
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-30 16:17:37 -0500

    added loading.jsx spinner and imported to ReposPage

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -5,13 +5,16 @@ async function fetchRepos() {
   const response = await fetch("https://api.github.com/users/4nth0ny1/repos", {
     next: { revalidate: 60 },
   });
+
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second for the loading.jsx spinner to show
+
   const repos = await response.json();
   return repos;
 }
 
 const ReposPage = async () => {
   const repos = await fetchRepos();
-  // console.log(repos); // since this is a server component, this console.log() will not show up in the client (browser), it will show up in the terminal.
+  console.log(repos[0]); // since this is a server component, this console.log() will not show up in the client (browser), it will show up in the terminal.
 
   return (
     <div className="repos-container">
