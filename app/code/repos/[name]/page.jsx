ae132c700857c7f6d4a7105eace750f8e5ab6205
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-30 17:01:23 -0500

    suspense boundary to Repo page and repodirs

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Repo from "../../../components/Repo";
+import Link from "next/link";
+import RepoDirs from "../../../components/RepoDirs";
+import { Suspense } from "react";
 
 const RepoDetailPage = ({ params }) => {
   const { name } = params;
 
   return (
     <div className="card">
-      <Repo name={name} />
+      <Link href="/code/repos" className="btn btn-back">
+        Back To Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   );
 };
